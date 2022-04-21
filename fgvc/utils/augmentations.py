ef698f8bfd122d67dc72a491b6e4679239607fab
Lukas Picek <lukaspicek@gmail.com>
2022-04-21 18:00:57 +0200

    Upsampling experiment added.

--- fgvc/utils/augmentations.py
+++ fgvc/utils/augmentations.py
@@ -1,5 +1,5 @@
 from albumentations.pytorch import ToTensorV2
-from albumentations import Compose, Resize, Normalize, RandomResizedCrop, HorizontalFlip, VerticalFlip, RandomBrightnessContrast, PadIfNeeded
+from albumentations import Compose, Resize, Normalize, RandomResizedCrop, HorizontalFlip, VerticalFlip, RandomBrightnessContrast, PadIfNeeded, RandomCrop
 
 
 def light_transforms(*, data, image_size, mean, std):
@@ -16,6 +16,29 @@ def light_transforms(*, data, image_size, mean, std):
             ToTensorV2(),
         ])
 
+    elif data == 'valid':
+        return Compose([
+            PadIfNeeded(image_size[0], image_size[1]),
+            Resize(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    
+def light_transforms_rcrop(*, data, image_size, mean, std):
+    
+    assert data in ('train', 'valid')
+
+    if data == 'train':
+        return Compose([
+            PadIfNeeded(image_size[0], image_size[1]),
+            RandomCrop(image_size[0], image_size[1]),
+            HorizontalFlip(p=0.5),
+            VerticalFlip(p=0.5),
+            RandomBrightnessContrast(p=0.2),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    
     elif data == 'valid':
         return Compose([
             PadIfNeeded(image_size[0], image_size[1]),
