7c6c2530c6e26d41f7fcf6e814545c216f5a4f40
Lukas Picek <lukaspicek@gmail.com>
2022-04-25 17:31:36 +0200

    RandomCrop experiment test script added.

--- fgvc/utils/augmentations.py
+++ fgvc/utils/augmentations.py
@@ -1,5 +1,5 @@
 from albumentations.pytorch import ToTensorV2
-from albumentations import Compose, Resize, Normalize, RandomResizedCrop, HorizontalFlip, VerticalFlip, RandomBrightnessContrast, PadIfNeeded, RandomCrop
+from albumentations import Compose, Resize, Normalize, RandomResizedCrop, HorizontalFlip, VerticalFlip, RandomBrightnessContrast, PadIfNeeded, RandomCrop, CenterCrop
 
 
 def light_transforms(*, data, image_size, mean, std):
@@ -26,7 +26,7 @@ def light_transforms(*, data, image_size, mean, std):
     
 def light_transforms_rcrop(*, data, image_size, mean, std):
     
-    assert data in ('train', 'valid')
+    assert data in ('train', 'valid', 'valid-center-crop')
 
     if data == 'train':
         return Compose([
@@ -43,6 +43,81 @@ def light_transforms_rcrop(*, data, image_size, mean, std):
         return Compose([
             PadIfNeeded(image_size[0], image_size[1]),
             Resize(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    
+    elif data == 'valid-center-crop':
+        return Compose([
+            PadIfNeeded(image_size[0], image_size[1]),
+            CenterCrop(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    
+def test_transforms(*, data, image_size, mean, std):
+    assert data in ('vanilla', 'center_crop', 'tta1', 'tta2', 'tta3', 'tta4', 'tta5', 'tta6', 'tta7')
+
+    if data == 'vanilla':
+        return Compose([
+            Resize(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'center_crop':
+        return Compose([
+            PadIfNeeded(image_size[0], image_size[1]),
+            CenterCrop(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'tta1':
+        return Compose([
+            Resize(int(image_size[0] / 0.9), int(image_size[1] / 0.9)),
+            CenterCrop(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'tta2':
+        return Compose([
+            Resize(int(image_size[0] / 0.8), int(image_size[1] / 0.8)),
+            CenterCrop(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'tta3':
+        return Compose([
+            Resize(int(image_size[0] / 0.65), int(image_size[1] / 0.65)),
+            CenterCrop(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'tta4':
+        return Compose([
+            PadIfNeeded(image_size[0], image_size[1]),
+            CenterCrop(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'tta5':
+        return Compose([
+            Resize(image_size[0], image_size[1]),
+            VerticalFlip(p=1.0),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'tta6':
+        return Compose([
+            Resize(image_size[0], image_size[1]),
+            HorizontalFlip(p=1.0),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    elif data == 'tta7':
+        return Compose([
+            Resize(image_size[0], image_size[1]),
+            HorizontalFlip(p=1.0),
+            VerticalFlip(p=1.0),
             Normalize(mean=mean, std=std),
             ToTensorV2(),
         ])
