5af29e68917c50824ab33cc502c558d20e444735
Lukas Picek <lukaspicek@gmail.com>
2022-05-03 14:05:26 +0200

    New training scripts added.

--- fgvc/utils/augmentations.py
+++ fgvc/utils/augmentations.py
@@ -1,5 +1,6 @@
 from albumentations.pytorch import ToTensorV2
-from albumentations import Compose, Resize, Normalize, RandomResizedCrop, HorizontalFlip, VerticalFlip, RandomBrightnessContrast, PadIfNeeded, RandomCrop, CenterCrop
+from albumentations import Compose, Resize, Normalize, RandomResizedCrop, HorizontalFlip, VerticalFlip, RandomBrightnessContrast, JpegCompression
+from albumentations import PadIfNeeded, RandomCrop, CenterCrop, ShiftScaleRotate, RandomGridShuffle, Blur, Cutout, MultiplicativeNoise, HueSaturationValue
 
 
 def light_transforms(*, data, image_size, mean, std):
@@ -12,6 +13,34 @@ def light_transforms(*, data, image_size, mean, std):
             HorizontalFlip(p=0.5),
             VerticalFlip(p=0.5),
             RandomBrightnessContrast(p=0.2),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+
+    elif data == 'valid':
+        return Compose([
+            PadIfNeeded(image_size[0], image_size[1]),
+            Resize(image_size[0], image_size[1]),
+            Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ])
+    
+def heavy_transforms(*, data, image_size, mean, std):
+    assert data in ('train', 'valid')
+
+    if data == 'train':
+        return Compose([
+            RandomResizedCrop(image_size[0], image_size[1], scale=(0.7, 1.3)),
+            HorizontalFlip(p=0.5),
+            VerticalFlip(p=0.5),
+            ShiftScaleRotate(shift_limit=0.10, scale_limit=0.25, rotate_limit=90, p=.5),
+            JpegCompression(p=0.25, quality_lower=50, quality_upper=100),
+            Blur(blur_limit=(7, 7), p=0.1),
+            RandomGridShuffle(grid=(3, 3), p=0.1),
+            RandomBrightnessContrast(p=0.3),
+            HueSaturationValue(p=0.2),
+            MultiplicativeNoise(multiplier=[0.8, 1.2], elementwise=True, p=0.1),
+            Cutout(num_holes=15, max_h_size=20, max_w_size=20, fill_value=128, p=0.5),
             Normalize(mean=mean, std=std),
             ToTensorV2(),
         ])
