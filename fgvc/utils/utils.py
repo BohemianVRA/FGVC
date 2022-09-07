9f36257aa9cd9df2e4ed83cd387c2d2988da6d2a
Ray Chamidullin <chamidullinr@gmail.com>
2022-09-07 07:33:46 +0200

    Added set cuda device function.

--- fgvc/utils/utils.py
+++ fgvc/utils/utils.py
@@ -15,6 +15,26 @@ def timer(name, logger):
     logger.info(f"[{name}] done in {time.time() - t0:.0f} s.")
 
 
+def set_cuda_device(cuda_devices: str) -> torch.device:
+    """Set cpu/cuda device in PyTorch and return device instance.
+
+    Parameters
+    ----------
+    cuda_devices
+        String specification of devices to use.
+        E.g. use "" for cpu, "0" for cuda:0, or "0,1" for cuda:0 and cuda:1
+
+    Returns
+    -------
+    Device instance.
+    """
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(cuda_devices)
+    torch.cuda.device_count()  # set CUDA_VISIBLE_DEVICES in PyTorch
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info(f"Using device: {device}")
+    return device
+
+
 def set_random_seed(seed=777):
     """Set random seed.
 
