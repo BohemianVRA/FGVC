316c4024b56bdfb01250b3b2288ee088fea1aef2
Ray Chamidullin <chamidullinr@gmail.com>
2022-10-14 10:22:23 +0200

    Add segmentation losses and metrics

--- fgvc/core/metrics.py
+++ fgvc/core/metrics.py
@@ -1,7 +1,12 @@
 from typing import Optional, Tuple
 
 import numpy as np
-from sklearn.metrics import accuracy_score, f1_score, top_k_accuracy_score
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    multilabel_confusion_matrix,
+    top_k_accuracy_score,
+)
 
 
 def classification_scores(
@@ -44,3 +49,106 @@ def classification_scores(
         out = acc, acc_k, f1
 
     return out
+
+
+def binary_segmentation_tp_fp_fn_tn(
+    preds: np.ndarray, targs: np.ndarray, pos_label: int = 1
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Compute values of confusion matrix for binary segmentation.
+
+    Parameters
+    ----------
+    preds
+        Array [b, (2), h, w] with predicted values.
+    targs
+        Array [b, h, w] with target values.
+    pos_label
+        Class to report in the binary classification.
+
+    Returns
+    -------
+    tp
+        Array [b,] with True Positives for each sample.
+    fp
+        Array [b,] with False Positives for each sample.
+    fn
+        Array [b,] with False Negatives for each sample.
+    tn
+        Array [b,] with True Negatives for each sample.
+    """
+    assert len(targs.shape) == 3
+    assert pos_label in (0, 1)
+    if len(preds.shape) == 4:
+        preds = preds.argmax(1)
+    assert preds.shape == targs.shape
+    assert np.all(np.isin(preds, [0, 1])), "The method supports only binary classification"
+    assert np.all(np.isin(targs, [0, 1])), "The method supports only binary classification"
+
+    # reshape samples
+    num_samples = preds.shape[0]
+    _preds = preds.reshape(num_samples, -1)
+    _targs = targs.reshape(num_samples, -1)
+
+    # compute confusion matrices per each sample (sklearn)
+    mcm = multilabel_confusion_matrix(_targs, _preds, samplewise=True)
+    ((tn, fn), (fp, tp)) = mcm.T
+
+    # compute confusion matrices per each sample (custom faster but memory intensive implementation)
+    # binary implementation
+    # targs_bin = (_targs == pos_label).astype(targs.dtype)
+    # preds_bin = (_preds == pos_label).astype(preds.dtype)
+    # tp = (targs_bin * preds_bin).sum(axis=1)  # true positive
+    # fp = ((1 - targs_bin) * preds_bin).sum(axis=1)  # false positive
+    # fn = (targs_bin * (1 - preds_bin)).sum(axis=1)  # false negative
+    # tn = ((1 - targs_bin) * (1 - preds_bin)).sum(axis=1)  # true negative
+
+    return tp, fp, fn, tn
+
+
+def binary_segmentation_scores(preds: np.ndarray, targs: np.ndarray, reduction: str = "mean") -> dict:
+    """Compute segmentation scores Balanced Accuracy, Precision, Recall, F1, and IoU.
+
+    Parameters
+    ----------
+    preds
+        Array with predicted values.
+    targs
+        Array with target values.
+    reduction
+        Whether to aggregate scores using mean, sum, or not aggregate at all.
+
+    Returns
+    -------
+    scores
+        A dictionary with segmentation scores.
+    """
+
+    def divide(a, b):
+        return np.divide(a, b, out=np.zeros_like(a, dtype=float), where=b != 0)
+
+    assert reduction in ("mean", "sum", None)
+
+    # compute batch-wise confusion matrices
+    tp, fp, fn, tn = binary_segmentation_tp_fp_fn_tn(preds, targs, pos_label=1)
+
+    # precision, recall, specificity
+    p = divide(tp, (tp + fp))
+    r = divide(tp, (tp + fn))  # sensitivity
+    spec = divide(tn, (tn + fp))
+
+    # compute scores
+    scores = {
+        # "acc": (tp + tn) / (tp + tn + fp + fn)
+        "balanced_acc": (r + spec) / 2,
+        "recall": r,
+        "precision": p,
+        # "specificity": spec,
+        "f1": divide((2 * p * r), (p + r)),
+        "iou": divide(tp, (tp + fp + fn)),
+    }
+    if reduction == "mean":
+        scores = {k: v.mean() for k, v in scores.items()}
+    elif reduction == "sum":
+        scores = {k: v.sum() for k, v in scores.items()}
+
+    return scores
