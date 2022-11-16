49a8f95acdede844f0f80ee541854cf7d62b1a9c
Ray Chamidullin <chamidullinr@gmail.com>
2022-11-16 11:30:12 +0100

    Improve setup.cfg

--- fgvc/__init__.py
+++ fgvc/__init__.py
@@ -1,6 +1,10 @@
 import logging
 
-from fgvc.utils.log import setup_logging
+from .utils.log import setup_logging
+from .version import __version__
+from . import core, datasets, losses, special, utils
+
+__all__ = ["core", "datasets", "losses", "special", "utils", "__version__"]
 
 setup_logging()
 logger = logging.getLogger("fgvc")
