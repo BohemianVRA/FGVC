0d52e29802d239041b8b1cce73c08cc1f3518d36
Ray Chamidullin <chamidullinr@gmail.com>
2022-11-16 11:47:43 +0100

    Update isort configuration

--- fgvc/__init__.py
+++ fgvc/__init__.py
@@ -1,8 +1,8 @@
 import logging
 
+from . import core, datasets, losses, special, utils
 from .utils.log import setup_logging
 from .version import __version__
-from . import core, datasets, losses, special, utils
 
 __all__ = ["core", "datasets", "losses", "special", "utils", "__version__"]
 
