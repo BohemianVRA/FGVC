f7923d761b61ef2c95d4760b4a30f8d07dbe3f03
Ray Chamidullin <chamidullinr@gmail.com>
2022-12-01 06:35:32 +0100

    Update Grad-CAM method to support Vision Transformers (#21)
    * Update Grad CAM to support Vision Transformers (ViT and SwinT)
    
    * Improve get_model method, make target_size optional.
    
    * Update visualization function for grad cam
    
    * Add strict argument to get_model function

--- fgvc/core/models.py
+++ fgvc/core/models.py
@@ -5,28 +5,52 @@ import torch.nn as nn
 
 def get_model(
     architecture_name: str,
-    target_size: int,
+    target_size: int = None,
     pretrained: bool = False,
+    *,
     checkpoint_path: str = None,
+    strict: bool = True,
 ) -> nn.Module:
-    """TODO add docstring."""
+    """Get a `timm` model.
+
+    Parameters
+    ----------
+    architecture_name
+        Name of the network architecture from `timm` library.
+    target_size
+        If set the model's classification head is replaced
+        with a classification head with output feature size = `target_size`.
+    pretrained
+        If true load pretrained ImageNet-1k weights.
+    checkpoint_path
+        Path of checkpoint to load after the model is initialized.
+    strict
+        Whether to strictly enforce the keys in state_dict to match
+        between the model and checkpoint weights from file.
+        Used when argument checkpoint_path is specified.
+
+    Returns
+    -------
+    model
+        PyTorch `nn.Module` instance.
+    """
     model = timm.create_model(architecture_name, pretrained=pretrained and checkpoint_path is None)
 
     # set classification head
-    model_cfg = model.default_cfg
-    cls_name = model_cfg["classifier"]
-    # iterate through nested modules
-    parts = cls_name.split(".")
-    module = model
-    for i, part_name in enumerate(parts):
-        if i == len(parts) - 1:
-            last_layer = getattr(module, part_name)
-            setattr(module, part_name, nn.Linear(last_layer.in_features, target_size))
-        else:
-            module = getattr(module, part_name)
+    if target_size is not None:
+        cls_name = model.default_cfg["classifier"]
+        # iterate through nested modules
+        parts = cls_name.split(".")
+        module = model
+        for i, part_name in enumerate(parts):
+            if i == len(parts) - 1:
+                last_layer = getattr(module, part_name)
+                setattr(module, part_name, nn.Linear(last_layer.in_features, target_size))
+            else:
+                module = getattr(module, part_name)
 
     # load custom weights
     if checkpoint_path is not None:
-        model.load_state_dict(torch.load(checkpoint_path, map_location="cpu"))
+        model.load_state_dict(torch.load(checkpoint_path, map_location="cpu"), strict=strict)
 
     return model
--- fgvc/special/grad_cam.py
+++ fgvc/special/grad_cam.py
@@ -1,11 +1,14 @@
+import math
 from typing import Callable, Tuple, Union
 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from PIL import Image
+
+from fgvc.core.augmentations import light_transforms
 
 
 def _minmax_normalize(x: np.ndarray, eps: float = 1e-6) -> np.ndarray:
@@ -17,16 +20,23 @@ def _minmax_normalize(x: np.ndarray, eps: float = 1e-6) -> np.ndarray:
 class GradCamTimm:
     def __init__(self, model: nn.Module, target_layer: str = None, device: torch.device = None):
         self.model = model
+        if target_layer is not None:
+            target_layers = self.get_possible_target_layers()
+            assert (
+                target_layer in target_layers
+            ), f"Unknown target layer '{target_layer}'. Use one of: {target_layers}"
         self.target_layer = target_layer
         if device is None:
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.device = device
 
-        self.gradients = None
-        self.features = None
+        self._gradients = None
 
     def __call__(
-        self, image: torch.Tensor, target_cls: int = None, reduction: Union[str, callable] = "mean"
+        self,
+        image: Union[torch.Tensor, np.ndarray],
+        target_cls: int = None,
+        reduction: Union[str, callable] = "mean",
     ) -> Tuple[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
         """Get the attentions for the timm model.
 
@@ -54,6 +64,9 @@ class GradCamTimm:
             isinstance(reduction, str) and reduction.lower() in allowed_reductions
         ), f"Argument `reduction` should be one of {allowed_reductions}, or a callable function. "
 
+        if isinstance(image, np.ndarray):
+            image = self.image_to_torch(image)
+
         self.model.eval()
         self.model = self.model.to(self.device)
         image = image.to(self.device)
@@ -67,11 +80,19 @@ class GradCamTimm:
         ), f"Allowed input shape is (1, channel, width, height): {image.shape}"
 
         # forward features and backward target
-        feats, logits = self.forward_pass(image)
-        grads = self.backward_pass(logits, target_cls)
+        features, logits = self.forward_pass(image)
+        gradients = self.backward_pass(logits, target_cls)
+
+        # remove batch dimension and convert features and gradients to numpy
+        features = features.cpu().detach().numpy()
+        gradients = gradients.cpu().detach().numpy()
+
+        if len(gradients.shape) == 3:
+            # apply changes to Vision Transformer embeddings
+            features, gradients = self.postprocess_transformer_embeddings(features, gradients)
 
         # return weighted features (attentions)
-        weighted_features = self.weight_features(feats, grads)
+        weighted_features = self.weight_features(features, gradients)
         if isinstance(reduction, str):
             # return_method parameter as string
             if "mean" in reduction.lower():
@@ -89,42 +110,68 @@ class GradCamTimm:
             weighted_features = reduction(weighted_features)
 
         # return the features and gradients (useful for visualization)
-        feats = np.mean(self.get_features(), axis=0)
-        grads = np.mean(self.get_gradients(), axis=0)
+        features = features[0].mean(axis=0)
+        gradients = gradients[0].mean(axis=0)
 
-        return weighted_features, (feats, grads)
+        return weighted_features, (features, gradients)
+
+    def get_possible_target_layers(self) -> list:
+        """Get a list of all possible values to use as `target_layer` argument."""
+        return [m for m, _ in self.model._modules.items()]
+
+    def image_to_torch(self, image: np.ndarray) -> torch.Tensor:
+        """Convert input NumPy image to a torch Tensor using test-time augmentations."""
+        # create pytorch image
+        assert hasattr(
+            self.model, "default_cfg"
+        ), "Model should have default_cfg dictionary with input_size, mean, and std."
+        _, tfm = light_transforms(
+            image_size=self.model.default_cfg["input_size"][1:],
+            mean=self.model.default_cfg["mean"],
+            std=self.model.default_cfg["std"],
+        )
+        image_torch = tfm(image=image)["image"]
+        return image_torch
 
     def _save_gradients(self, gradients: torch.Tensor):
         """Tensor hook for saving gradients."""
-        self.gradients = gradients
+        self._gradients = gradients
 
-    def forward_pass(self, x: torch.Tensor) -> Union[torch.Tensor, torch.Tensor]:
+    def forward_pass(self, image: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """Run forward pass and return encoder features and classifier logits.
 
         Parameters
         ----------
-        x
+        image
             Input to the model as single image or batch of one image.
 
         Returns
         -------
-        self.features
+        features
             Extracted encoder features.
-        x
+        logits
             Classifier logits.
         """
-        assert x.shape[0] == 1, f"Allowed input shape is (1, channel, width, height): {x.shape}"
+        assert (
+            image.shape[0] == 1
+        ), f"Allowed input shape is (1, channel, width, height): {image.shape}"
         if self.target_layer is None:
             # forward features through the conv layers
-            x = self.model.forward_features(x)
             # save the features and gradients from the last conv
-            self.features = x
-            x.register_hook(self._save_gradients)
+            features = self.model.forward_features(image)
+            features.register_hook(self._save_gradients)
 
             # forward features through the classification head
-            x = self.model.forward_head(x)
+            logits = self.model.forward_head(features)
         else:
+            target_layers = self.get_possible_target_layers()
+            assert (
+                self.target_layer in target_layers
+            ), f"Unknown target layer '{self.target_layer}'. Use one of: {target_layers}"
+
+            x = image
             break_for = False
+            features, logits = None, None
             for m, module in self.model._modules.items():
                 # forward features
                 if m in ["head", "global_pool"]:
@@ -137,14 +184,16 @@ class GradCamTimm:
 
                 # save the features and gradients from the last conv
                 if str(m) == str(self.target_layer):
-                    self.features = x
-                    x.register_hook(self._save_gradients)
+                    features = x
+                    features.register_hook(self._save_gradients)
 
                 # features was passed through the classification head
                 if break_for:
+                    logits = x
                     break
+            assert features is not None and logits is not None
 
-        return self.features, x
+        return features, logits
 
     def backward_pass(self, logits: torch.Tensor, target_cls: int = None) -> torch.Tensor:
         """Run backward pass and return gradients w.r.t. target class.
@@ -161,7 +210,7 @@ class GradCamTimm:
 
         Returns
         -------
-        self.gradients
+        gradients
             Evaluated gradient tensor w.r.t. target class.
         """
         assert logits.shape[0] == 1, f"Allowed logits shape is (1, num_classes): {logits.shape}"
@@ -176,12 +225,41 @@ class GradCamTimm:
         # zero gradients and backward the target to get the gradient
         self.model.zero_grad()
         logits.backward(gradient=y, retain_graph=True)
+        gradients = self._gradients
 
-        return self.gradients
+        return gradients
 
-    def weight_features(
-        self, features: Union[np.ndarray, torch.Tensor], gradients: Union[np.ndarray, torch.Tensor]
-    ) -> np.ndarray:
+    def postprocess_transformer_embeddings(
+        self, features: np.ndarray, gradients: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """Postprocess Vision Transformer embeddings to make them spatial."""
+        # reshape embedding features into spatial map to make output similar to CNNs
+        bs, patch_dim, emb_dim = gradients.shape
+        sqrt = math.sqrt(patch_dim)
+        if sqrt % 1 != 0:
+            sqrt = math.sqrt(patch_dim - 1)
+            if sqrt % 1 == 0:
+                # ignore first element (positional embedding) in the output embedding
+                features = features[:, 1:]
+                gradients = gradients[:, 1:]
+            else:
+                raise ValueError(f"Unsupported patch dimension: {patch_dim}")
+        sqrt = int(sqrt)
+        # [bs, patch_dim, emb_dim] -> [bs, width, height, emb_dim]
+        features = features.reshape(bs, sqrt, sqrt, emb_dim)
+        gradients = gradients.reshape(bs, sqrt, sqrt, emb_dim)
+
+        # [bs, width, height, emb_dim] -> [bs, emb_dim, width, height]
+        features = features.transpose(0, 3, 1, 2)
+        gradients = gradients.transpose(0, 3, 1, 2)
+
+        # # [bs, patch_dim, emb_dim] -> [bs, emb_dim, width, height]
+        # features = features.reshape(bs, emb_dim, sqrt, sqrt)
+        # gradients = gradients.reshape(bs, emb_dim, sqrt, sqrt)
+
+        return features, gradients
+
+    def weight_features(self, features: np.ndarray, gradients: np.ndarray) -> np.ndarray:
         """Combing features with gradients.
 
         Parameters
@@ -197,104 +275,105 @@ class GradCamTimm:
             Weighted Features (attentions).
         """
         assert features.shape == gradients.shape
-        assert len(features.shape) == len(gradients.shape) == 4
-        assert features.shape[0] == 1, f"Allowed features shape is (1, ...): {features.shape}"
-        if isinstance(features, torch.Tensor):
-            features = features.cpu().detach().numpy()
-        if isinstance(gradients, torch.Tensor):
-            gradients = gradients.cpu().detach().numpy()
+        assert len(features.shape) == len(gradients.shape) == 4, (
+            len(features.shape),
+            len(gradients.shape),
+        )
+        assert (
+            features.shape[0] == 1
+        ), f"Allowed features shape is (1, channel, width, height): {features.shape}"
 
-        # take only the first image from the batch -> batch size must be 1!
-        features = features[0]
-        gradients = gradients[0]
+        # take only the first image from the batch
+        features, gradients = features[0], gradients[0]
 
-        # average through width and height -> getting weight for channels
-        weights = np.mean(gradients, axis=(1, 2))
+        # average through width and height in vector of shape [channel, width, height]
+        weights = np.mean(gradients, axis=(1, 2), keepdims=True)
 
-        # (channel, 1, 1) -> (channel, width, height)
-        weights = np.expand_dims(weights, axis=(-1, -2))
-
+        # weight features with gradient average weights
         weighted_features = weights * features
+
         return weighted_features
 
-    def get_possible_target_layers(self) -> list:
-        """Get a list of all possible values to use as `target_layer` argument."""
-        return [m for m, _ in self.model._modules.items()]
 
-    def set_target_layer(self, target_layer: str):
-        """Set `target_layer`."""
-        self.target_layer = target_layer
+def plot_heatmap(
+    features: np.ndarray,
+    *,
+    ax=None,
+    cmap: str = "viridis",
+    scale_format: str = "%.4f",
+):
+    """Plot features, gradients, or weighted features (attentions) as a heatmap."""
+    assert len(features.shape) == 2
 
-    def get_features(self) -> np.ndarray:
-        """Get NumPy array with extracted features from a forward pass."""
-        return self.features[0].cpu().detach().numpy()
+    # plot results
+    if ax is None:
+        ax = plt.gca()
+    im = ax.imshow(features, cmap=cmap)
+    plt.colorbar(
+        im,
+        ticks=np.linspace(features.min(), features.max(), 5, endpoint=True),
+        format=scale_format,
+    )
+    ax.tick_params(labelsize="xx-small")
+    return ax
 
-    def get_gradients(self) -> np.ndarray:
-        """Get NumPy array with evaluated gradients from a backward pass."""
-        return self.gradients[0].cpu().detach().numpy()
 
-    @staticmethod
-    def visualize_as_heatmap(
-        features: Union[np.ndarray, torch.Tensor],
-        *,
-        ax=None,
-        cmap: str = "viridis",
-        num_ticks: int = 5,
-        scale_format: str = "%.4f",
-        labelsize: str = "xx-small",
-    ):
-        """Visualize attention as heatmap."""
-        if isinstance(features, torch.Tensor):
-            features = features.cpu().detach().numpy()
+def plot_image_heatmap(
+    weighted_features: np.ndarray,
+    image: np.ndarray,
+    max_value: float = 1,
+    *,
+    ax=None,
+    scale_format: str = "%.4f",
+    use_shadow: bool = False,
+    use_min_zero: bool = True,
+):
+    """Plot image with features, gradients, or weighted features (attentions) as a heatmap."""
+    assert len(weighted_features.shape) == 2
+    assert len(image.shape) in (2, 3)
 
-        # plot results
-        if ax is None:
-            ax = plt.gca()
-        im = ax.imshow(features, cmap=cmap)
-        plt.colorbar(
-            im,
-            ticks=np.linspace(np.min(features), np.max(features), num_ticks, endpoint=True),
-            format=scale_format,
+    # process image
+    image = _minmax_normalize(image)
+
+    # process weighted features
+    h, w = image.shape[:2]
+    if use_min_zero:
+        weighted_features = np.maximum(weighted_features, 0)
+    weighted_features = _minmax_normalize(weighted_features)
+    weighted_features = cv2.resize(weighted_features, (w, h))
+
+    # combine image with weighted features
+    if use_shadow:
+        if len(image.shape) == 3:
+            weighted_features = weighted_features[..., None]
+        cam = image * weighted_features
+    else:
+        # create heatmap of weighted features
+        heatmap = cv2.applyColorMap(
+            np.uint8(255 * weighted_features * max_value), cv2.COLORMAP_TURBO
         )
-        ax.tick_params(labelsize=labelsize)
+        heatmap = np.float32(heatmap) / 255
+        heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
 
-    @staticmethod
-    def visualize_as_image(
-        weighted_features: Union[np.ndarray, torch.Tensor],
-        image: Union[np.ndarray, torch.Tensor],
-        *,
-        ax=None,
-        rescale_only: bool = False,
-    ):
-        """Visualize attention as image."""
-        assert len(image.shape) == 3
-        if isinstance(image, torch.Tensor):
-            image = image.cpu().detach().numpy()
+        cam = image + heatmap
+        cam = cam / cam.max()
 
-        # scale between 0-255
-        cam = np.maximum(weighted_features, 0)
-        cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))
-        cam = np.uint8(cam * 255)
-
-        # resize into the image shape
-        ch, h, w = image.shape
-        cam = np.uint8(Image.fromarray(cam).resize((w, h), Image.ANTIALIAS)) / 255
-
-        # if required, show attentions in the image
-        if not rescale_only:
-            cam = cam * image
-            if min(cam.shape) == cam.shape[0]:
-                # channel last
-                cam = np.moveaxis(cam, 0, -1)
-
-        # plot results
-        if ax is None:
-            ax = plt.gca()
-        ax.imshow(cam)
+    # plot results
+    if ax is None:
+        ax = plt.gca()
+    ax.imshow(cam)
+    # if not use_shadow:
+    #     plt.colorbar(
+    #         im,
+    #         ticks=np.linspace(weighted_features.min(), weighted_features.max(), 5, endpoint=True),
+    #         format=scale_format,
+    #     )
+    #     ax.tick_params(labelsize="xx-small")
+    return ax
 
 
 def plot_grad_cam(
-    image: torch.Tensor,
+    image: np.ndarray,
     model: nn.Module,
     *,
     target_layer: str = None,
@@ -303,6 +382,7 @@ def plot_grad_cam(
     reduction: Union[str, callable] = "mean",
     colsize: int = 5,
     rowsize: int = 4,
+    use_min_zero: bool = True,
 ):
     """Apply Grad-CAM and visualize the results.
 
@@ -310,44 +390,49 @@ def plot_grad_cam(
     """
     # create Grad-CAM instance and get the attentions
     grad_cam = GradCamTimm(model, target_layer, device)
-    attn, (feats, grads) = grad_cam(image, target_cls, reduction)
-
-    # create numpy image
-    image_np = np.uint8(image.permute(1, 2, 0).cpu().numpy() * 255)
+    weighted_features, (features, gradients) = grad_cam(image, target_cls, reduction)
 
     # create figure to visualize the attentions
-    nrows, ncols = 3, 2
-    fig = plt.figure(figsize=(ncols * colsize, nrows * rowsize), tight_layout=True)
+    nrows, ncols = 2, 3
+    fig = plt.figure(figsize=(ncols * colsize, nrows * rowsize), layout="tight")
     gs = fig.add_gridspec(nrows, ncols)
 
-    # show the input image
-    ax1 = fig.add_subplot(gs[0, :])
-    ax1.set_title("Input Image")
-    ax1.imshow(image_np)
-    ax1.axis("off")
+    # plot the input image
+    ax11 = fig.add_subplot(gs[0, 0])
+    ax11.imshow(image)
+    ax11.set(title="Input image")
+    ax11.axis("off")
 
-    # show the attentions as heatmap
-    ax21 = fig.add_subplot(gs[1, 0])
-    ax21.set_title("Model's Attentions")
-    grad_cam.visualize_as_heatmap(attn, labelsize="small", ax=ax21)
-    ax21.axis("off")
+    # plot the input image with the attentions as heatmap
+    ax12 = fig.add_subplot(gs[0, 1])
+    plot_image_heatmap(weighted_features, image, ax=ax12, use_min_zero=use_min_zero)
+    ax12.set(title="Input image with attentions (heatmap)")
+    ax12.axis("off")
+
+    # plot the input image with the attentions as shadow
+    ax13 = fig.add_subplot(gs[0, 2])
+    plot_image_heatmap(
+        weighted_features, image, ax=ax13, use_shadow=True, use_min_zero=use_min_zero
+    )
+    ax13.set(title="Input image with attentions (shadow)")
+    ax13.axis("off")
 
     # show resized attentions and applied on the image
-    ax22 = fig.add_subplot(gs[2, 0])
-    ax22.set_title("Applied Attentions on Image")
-    grad_cam.visualize_as_image(attn, image, ax=ax22)
-    ax22.axis("off")
+    ax21 = fig.add_subplot(gs[1, 0])
+    ax21.set(title="Features")
+    plot_heatmap(features, ax=ax21)
+    ax21.axis("off")
 
     # show original features
     ax31 = fig.add_subplot(gs[1, 1])
-    ax31.set_title("Features of the Last Conv")
-    grad_cam.visualize_as_heatmap(feats, labelsize="small", ax=ax31)
+    ax31.set(title="Gradients")
+    plot_heatmap(gradients, ax=ax31)
     ax31.axis("off")
 
     # show original gradients
-    ax32 = fig.add_subplot(gs[2, 1])
-    ax32.set_title("Gradients (const causes the global pooling)")
-    grad_cam.visualize_as_heatmap(grads, labelsize="small", ax=ax32)
+    ax32 = fig.add_subplot(gs[1, 2])
+    ax32.set(title="Weighted features (attentions)")
+    plot_heatmap(weighted_features, ax=ax32)
     ax32.axis("off")
 
     plt.show()
