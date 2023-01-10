079475f91883344447c44dfe7abb76b935396ac0
Ray Chamidullin <chamidullinr@gmail.com>
2023-01-10 02:07:37 +0100

    Update functionality of EMA (make it run on all epochs, not just last % like SWA)

--- fgvc/core/training/scheduler_mixin.py
+++ fgvc/core/training/scheduler_mixin.py
@@ -16,6 +16,10 @@ class SchedulerMixin:
 
     The SchedulerMixin supports PyTorch and timm schedulers.
 
+    The SchedulerMixin supports model weight averaging, namely two strategies:
+    - Stochastic Weight Averaging - applied on the last `swa_epochs` with a dedicated LR scheduler.
+    - Exponential Moving Averaging - applied on all epochs with any scheduler.
+
     Parameters
     ----------
     scheduler
@@ -83,6 +87,7 @@ class SchedulerMixin:
             assert swa in ("swa", "ema")
             assert isinstance(swa_epochs, int) or 0.0 < swa_epochs < 1.0
             assert trainloader is not None
+            self.swa = swa
             self.swa_epochs = swa_epochs
             if swa == "swa":
                 self.swa_model = AveragedModel(model)
@@ -118,55 +123,8 @@ class SchedulerMixin:
         if self.scheduler is not None and isinstance(self.scheduler, CosineLRScheduler):
             self.scheduler.step_update(num_updates=num_updates)
 
-    def make_scheduler_step(
-        self, epoch: int = None, *, valid_loss: float = None, num_epochs: int = None
-    ):
-        """Make scheduler step after training one epoch.
-
-        The method uses different arguments depending on the scheduler type.
-
-        Parameters
-        ----------
-        epoch
-            Current epoch number. The method expects start index 1 (instead of 0).
-        valid_loss
-            Average validation loss to use for `ReduceLROnPlateau` scheduler.
-        num_epochs
-            Number of epochs to train.
-        """
-        # make SWA or EMA step
-        apply_swa = False
-        if self.swa_model is not None:
-            if num_epochs is None:
-                warnings.warn(
-                    f"Trainer has enabled SWA strategy with swa_epochs={self.swa_epochs} "
-                    f"but method `make_scheduler_step` did not get `num_epochs` and cannot "
-                    "establish SWA start epoch or last training epoch."
-                )
-
-            # decide if to apply SWA or EMA
-            abs_swa_epochs = self.swa_epochs
-            if 0.0 < self.swa_epochs < 1.0:
-                if num_epochs is None:
-                    # set very large number to make following conditions false
-                    abs_swa_epochs = 100_000
-                else:
-                    # convert relative swa_epochs to absolute value
-                    abs_swa_epochs = int(self.swa_epochs * num_epochs)
-            apply_swa = epoch >= abs_swa_epochs
-
-            # apply SWA or EMA
-            if apply_swa:
-                self.swa_model.update_parameters(self.model)
-                if self.swa_scheduler is not None:
-                    self.swa_scheduler.step()  # apply SWA
-
-        # update bn statistics for the swa_model at the end
-        if num_epochs is not None and epoch == num_epochs:
-            update_bn(self.trainloader, self.swa_model)
-
-        # make scheduler step
-        if self.scheduler is not None and (not apply_swa or self.swa_scheduler is None):
+    def _make_scheduler_step(self, epoch: int, valid_loss: float):
+        if self.scheduler is not None:
             if isinstance(self.scheduler, ReduceLROnPlateau):
                 if valid_loss is not None:
                     self.scheduler.step(valid_loss)  # pytorch implementation
@@ -186,3 +144,58 @@ class SchedulerMixin:
                     )
             else:
                 raise ValueError(f"Unsupported scheduler type: {self.scheduler}")
+
+    def make_scheduler_step(
+        self, epoch: int = None, *, valid_loss: float = None, num_epochs: int = None
+    ):
+        """Make scheduler step after training one epoch.
+
+        The method uses different arguments depending on the scheduler type.
+
+        Parameters
+        ----------
+        epoch
+            Current epoch number. The method expects start index 1 (instead of 0).
+        valid_loss
+            Average validation loss to use for `ReduceLROnPlateau` scheduler.
+        num_epochs
+            Number of epochs to train.
+        """
+        if self.swa_model is None:
+            # make scheduler step
+            self._make_scheduler_step(epoch, valid_loss)
+        else:
+            # apply SWA or EMA
+            if num_epochs is None:
+                warnings.warn(
+                    f"Trainer has enabled SWA strategy with swa_epochs={self.swa_epochs} "
+                    f"but method `make_scheduler_step` did not get `num_epochs` and cannot "
+                    "establish SWA start epoch or last training epoch."
+                )
+
+            if self.swa == "ema":
+                # apply EMA
+                self.swa_model.update_parameters(self.model)
+
+                # make scheduler step
+                self._make_scheduler_step(epoch, valid_loss)
+            else:
+                # decide if to apply SWA based on the current epoch
+                abs_swa_epochs = self.swa_epochs
+                if 0.0 < self.swa_epochs < 1.0:
+                    if num_epochs is None:
+                        # set very large number to make following conditions false
+                        # this issue is notified in a warning above
+                        abs_swa_epochs = 100_000
+                    else:
+                        # convert relative swa_epochs to absolute value
+                        abs_swa_epochs = int(self.swa_epochs * num_epochs)
+
+                # apply SWA
+                if epoch >= abs_swa_epochs:
+                    self.swa_model.update_parameters(self.model)
+                    self.swa_scheduler.step()
+
+            # update bn statistics for the swa_model at the end
+            if num_epochs is not None and epoch == num_epochs:
+                update_bn(self.trainloader, self.swa_model)
