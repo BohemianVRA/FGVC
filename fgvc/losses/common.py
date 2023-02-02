f669a5bb2ea8eb2220775a0803aaa3e1920c263b
Ray Chamidullin <chamidullinr@gmail.com>
2023-02-02 01:51:05 +0100

    Add normalizing factor to Compose Loss

--- fgvc/losses/common.py
+++ fgvc/losses/common.py
@@ -1,6 +1,5 @@
 from typing import List
 
-import numpy as np
 import torch
 import torch.nn as nn
 
@@ -9,7 +8,9 @@ class ComposeLoss(nn.Module):
     def __init__(self, criterions: List[nn.Module], weights: List[float] = None):
         super().__init__()
         if weights is None:
-            weights = np.ones(len(criterions))
+            weights = torch.ones(len(criterions))
+        else:
+            weights = torch.as_tensor(weights)
         assert len(criterions) > 1
         assert len(criterions) == len(weights), "Each loss should have a corresponding weights."
         self.criterions = criterions
@@ -17,7 +18,9 @@ class ComposeLoss(nn.Module):
 
     def forward(self, logits: torch.Tensor, targs: torch.Tensor) -> torch.Tensor:
         """Compute individual losses and combine them together."""
+        self.weights.to(logits.device)
         loss = 0
         for criterion, weight in zip(self.criterions, self.weights):
             loss += weight * criterion(logits, targs)
+        loss = len(self.weights) * loss / self.weights.sum()
         return loss
