0b1bc41b5be3837a978776b1642e699999159f9c
Ray Chamidullin <chamidullinr@gmail.com>
2023-02-06 03:53:09 +0100

    Update test CLI script to create report tables in W&B

--- fgvc/cli/test.py
+++ fgvc/cli/test.py
@@ -3,12 +3,18 @@ import os
 import sys
 
 import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report
 
 from fgvc.core.training import predict
 from fgvc.datasets import get_dataloaders
+from fgvc.special.threshold_analysis import (
+    class_wise_confidence_threshold_report,
+    estimate_optimal_confidence_thresholds,
+)
 from fgvc.utils.experiment import get_experiment_path, load_args, load_model, load_test_metadata
 from fgvc.utils.utils import set_cuda_device
-from fgvc.utils.wandb import log_clf_test_scores, wandb
+from fgvc.utils.wandb import log_clf_test_scores, resume_wandb, wandb
 
 logger = logging.getLogger("script")
 
@@ -26,6 +32,59 @@ def add_arguments(parser):
         help="Re-runs evaluation on test set even if the run already has test scores.",
         action="store_true",
     )
+    parser.add_argument(
+        "--label-col",
+        help="Name of column with target classes (labels) in the test set.",
+        type=str,
+        default=None,
+    )
+
+
+def classification_report_df(
+    test_df: pd.DataFrame, preds: np.ndarray, targs: np.ndarray, label_col: str = None
+) -> pd.DataFrame:
+    """Create classification report with class-wise Precision, Recall, and F1 score metrics.
+
+    The method is based on the `sklearn.metrics.classification_report` method.
+    """
+    report_df = pd.DataFrame.from_dict(
+        classification_report(
+            targs,
+            preds.argmax(1),
+            labels=np.arange(preds.shape[1]),
+            zero_division=0,
+            output_dict=True,
+        ),
+        orient="index",
+    )
+    report_df.index.name = "class_id"
+    report_df = report_df.reset_index()
+    report_df = report_df.rename(
+        columns={
+            "precision": "Precision",
+            "recall": "Recall",
+            "f1-score": "F1",
+            "support": "Num Records",
+        }
+    )
+    if label_col is not None:
+        id2label = dict(zip(test_df["class_id"].astype(str), test_df[label_col]))
+        report_df.insert(1, label_col, report_df["class_id"].astype(str).apply(id2label.get))
+    return report_df
+
+
+def threhold_analysis_report_df(
+    test_df: pd.DataFrame, preds: np.ndarray, targs: np.ndarray, label_col: str = None
+) -> pd.DataFrame:
+    """Create threshold analysis report with class-wise confidence thresholds and metrics."""
+    confidence_thresholds = estimate_optimal_confidence_thresholds(preds, targs)
+    report_df = class_wise_confidence_threshold_report(preds, targs, confidence_thresholds)
+    report_df.index.name = "class_id"
+    report_df = report_df.reset_index()
+    if label_col is not None:
+        id2label = dict(zip(test_df["class_id"].astype(str), test_df[label_col]))
+        report_df.insert(1, label_col, report_df["class_id"].astype(str).apply(id2label.get))
+    return report_df
 
 
 def test_clf(
@@ -34,6 +93,7 @@ def test_clf(
     wandb_run_path: str = None,
     cuda_devices: str = None,
     rerun: str = None,
+    label_col: str = None,
     **kwargs,
 ):
     """Test model on the classification task and log test scores as a run summary in W&B."""
@@ -48,12 +108,15 @@ def test_clf(
         wandb_run_path = args.wandb_run_path
         cuda_devices = args.cuda_devices
         rerun = args.rerun
+        label_col = args.label_col
 
     # set device
     device = set_cuda_device(cuda_devices)
 
     # load metadata
     test_df = load_test_metadata(test_metadata)
+    if label_col is not None:
+        assert label_col in test_df, f"Test dataframe is missing column '{label_col}'."
 
     # connect to wandb and load run
     logger.info(f"Loading W&B experiment run: {wandb_run_path}")
@@ -107,6 +170,17 @@ def test_clf(
         test_acc3=scores["Recall@3"],
         test_f1=scores["F1"],
         allow_new=True,
+    )
+
+    # resume W&B run and log classification report to W&B
+    resume_wandb(run_id=run.id, entity=run.entity, project=run.project)
+    clf_report_df = classification_report_df(test_df, preds, targs, label_col=label_col)
+    th_report_df = threhold_analysis_report_df(test_df, preds, targs, label_col=label_col)
+    wandb.log(
+        {
+            "clf_report_table": wandb.Table(dataframe=clf_report_df),
+            "th_report_table": wandb.Table(dataframe=th_report_df),
+        }
     )
 
     # store predictions and targets in the experiment dir
