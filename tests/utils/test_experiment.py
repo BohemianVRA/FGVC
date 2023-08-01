3aded7836f4c9b5495d925d15ee20327e08946a6
Ray Chamidullin <chamidullinr@gmail.com>
2023-08-01 13:48:59 +0200

    test: add unit tests for load_config method

--- tests/utils/test_experiment.py
+++ tests/utils/test_experiment.py
@@ -1,8 +1,9 @@
 from typing import Optional
+from unittest.mock import MagicMock, mock_open, patch
 
 import pytest
 
-from fgvc.utils.experiment import load_train_args
+from fgvc.utils.experiment import load_config, load_train_args
 
 
 @pytest.mark.parametrize(
@@ -66,3 +67,105 @@ def test_load_train_args(
         assert extra_args.get("root-path") == target_root_path
     if target_architecture is not None:
         assert extra_args.get("architecture") == target_architecture
+
+
+@pytest.fixture(scope="module")
+def input_config():
+    return {
+        # data
+        "augmentations": "light",
+        "image_size": [224, 224],  # [height, width]
+        "dataset": "DF20-Mini",
+        # model
+        "architecture": "vit_base_patch32_224",
+        # training
+        "loss": "CrossEntropyLoss",
+        "optimizer": "SGD",
+        "scheduler": "cosine",
+        "epochs": 40,
+        "learning_rate": 0.001,
+        "batch_size": 64,
+        "accumulation_steps": 1,
+        # other
+        "random_seed": 777,
+        "workers": 4,
+        "multigpu": False,
+        "tags": ["baseline"],  # W&B Run tags
+    }
+
+
+@patch("builtins.open", new_callable=mock_open, read_data=None)
+@patch("fgvc.utils.experiment.yaml.safe_load")
+@patch("os.makedirs")
+def test_load_config_1(makedirs: MagicMock, safe_load: MagicMock, open_: MagicMock, input_config: dict):
+    """Test loading configuration YAML file and setting up experiment path."""
+    target_exp_path = "./runs/vit_base_patch32_224-CrossEntropyLoss-light/exp1"
+    safe_load.return_value = input_config
+    config = load_config(
+        config_path="dummy_config.yaml", run_name_fmt="architecture-loss-augmentations"
+    )
+    makedirs.assert_called_once_with(target_exp_path, exist_ok=False)
+    for k in input_config.keys():
+        assert input_config[k] == config[k], k
+    assert "exp_name" in config
+    assert config["exp_name"] == "exp1"
+    assert "exp_path" in config
+    assert config["exp_path"] == target_exp_path
+    assert "run_name" in config
+    assert config["run_name"] == "vit_base_patch32_224-CrossEntropyLoss-light"
+
+
+@patch("builtins.open", new_callable=mock_open, read_data=None)
+@patch("fgvc.utils.experiment.yaml.safe_load")
+@patch("os.makedirs")
+def test_load_config_2(makedirs: MagicMock, safe_load: MagicMock, open_: MagicMock, input_config: dict):
+    """Test loading configuration YAML file and setting up experiment path.
+
+    Test `run_name_fmt` method argument.
+    """
+    target_exp_path = "./runs/DF20-Mini-40-0.001/exp1"
+    safe_load.return_value = input_config
+    config = load_config(
+        config_path="dummy_config.yaml", run_name_fmt="dataset-epochs-learning_rate"
+    )
+    makedirs.assert_called_once_with(target_exp_path, exist_ok=False)
+    for k in input_config.keys():
+        assert input_config[k] == config[k], k
+    assert "exp_path" in config
+    assert config["exp_path"] == target_exp_path
+    assert "run_name" in config
+    assert config["run_name"] == "DF20-Mini-40-0.001"
+
+
+@patch("builtins.open", new_callable=mock_open, read_data=None)
+@patch("fgvc.utils.experiment.yaml.safe_load")
+@patch("os.makedirs")
+def test_load_config_3(makedirs: MagicMock, safe_load: MagicMock, open_: MagicMock, input_config: dict):
+    """Test loading configuration YAML file and setting up experiment path.
+
+    Test `root_path` config parameter.
+    """
+    # test root_path in config
+    input_config_ = {**input_config, "root_path": "/data/project1/"}
+    target_exp_path = "/data/project1/runs/vit_base_patch32_224-CrossEntropyLoss-light/exp1"
+    safe_load.return_value = input_config_
+    config = load_config(
+        config_path="dummy_config.yaml", run_name_fmt="architecture-loss-augmentations"
+    )
+    makedirs.assert_called_once_with(target_exp_path, exist_ok=False)
+    for k in input_config_.keys():
+        assert input_config_[k] == config[k], k
+    assert "exp_path" in config
+    assert config["exp_path"] == target_exp_path
+
+    # test root_path in method argument
+    target_exp_path = "/data/project1/runs/vit_base_patch32_224-CrossEntropyLoss-light/exp1"
+    safe_load.return_value = input_config
+    config = load_config(
+        config_path="dummy_config.yaml", run_name_fmt="architecture-loss-augmentations",
+        extra_args={"root_path": "/data/project1/"}
+    )
+    for k in input_config_.keys():
+        assert input_config_[k] == config[k], k
+    assert "exp_path" in config
+    assert config["exp_path"] == target_exp_path
